6b03e0513c28c1ba48147d9c28f79803313ac28b
Vuldislav <98829443+vladnory@users.noreply.github.com>
2023-10-24 14:56:34 +0300

    added welcome page (desktop)

--- src/features/welcome/components/Welcome.tsx
+++ src/features/welcome/components/Welcome.tsx
@@ -1,10 +1,25 @@
-import { Link } from "react-router-dom";
+import WelcomeLogo from "assets/images/handFingerUp.svg";
+import Button from "components/Button/Button";
+import { useNavigate } from "react-router-dom";
 
 function Welcome() {
+  const navigate = useNavigate();
+  const handleGoToGame = () => navigate("/who-wants-to-be-a-millionaire");
+
   return (
-    <div className="diagonal-split">
-      <p>Who wants to be a millionaire?</p>
-      <Link to="/who-wants-to-be-a-millionaire">Start</Link>
+    <div className="fullscreen centered diagonal-split welcome__container">
+      <div className="welcome__container_item centered">
+        <img src={WelcomeLogo} alt="page logo" />
+      </div>
+      <div className="welcome__container_item centered">
+        <p className="welcome__container_slogan">
+          Who wants to be <br />a millionaire?
+        </p>
+
+        <Button onClick={handleGoToGame} variant="desktop">
+          Start
+        </Button>
+      </div>
     </div>
   );
 }
