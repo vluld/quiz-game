5657927bcf86eaaa2f1c2a51e85264e1a1a4a619
Vuldislav <98829443+vladnory@users.noreply.github.com>
2023-10-25 11:31:15 +0300

    added test to result page

--- src/features/results/components/tests/Results.test.tsx
+++ src/features/results/components/tests/Results.test.tsx
@@ -1,9 +1,35 @@
+import { configureStore } from "@reduxjs/toolkit";
 import { screen } from "@testing-library/react";
+import quizQuestions from "data/quizData.json";
+import {
+  answerQuestion,
+  checkResult,
+  startGame,
+} from "features/quizGame/state/quizGameReducerSlice";
+import { reducer } from "state/reducer";
 import { renderApp } from "tests/setup";
 import Results from "../Results";
 
-test("renders result page", () => {
-  renderApp(<Results />);
-  const linkElement = screen.getByText(/millionaire/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Results page", () => {
+  const store = configureStore({
+    reducer,
+  });
+
+  test("renders earned amount on result page", () => {
+    store.dispatch(startGame(quizQuestions));
+    store.dispatch(answerQuestion(2));
+    store.dispatch(checkResult());
+    store.dispatch(answerQuestion(1));
+    store.dispatch(checkResult());
+    store.dispatch(answerQuestion(2));
+    store.dispatch(checkResult());
+
+    renderApp(<Results />, store);
+
+    const amountTitle = screen.getByText(/Earned/i);
+    expect(amountTitle).toBeInTheDocument();
+
+    const amount = screen.getByText(/\$300/i);
+    expect(amount).toBeInTheDocument();
+  });
 });
